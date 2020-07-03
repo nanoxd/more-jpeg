d73f0383299a9073bebfc260a089d499da11c188
Fernando Paredes <nano@fdp.io>
2020-07-03 13:01:28 -0400

    Setup Server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut app = tide::new();
+
+    app.at("/").get(|_| async { Ok("Hello from Tide") });
+
+    app.listen("localhost:3000").await?;
+    Ok(())
 }
