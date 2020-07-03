3bf138b83bf4effe48fe2e915ebaa90c7d024cc1
Fernando Paredes <nano@fdp.io>
2020-07-03 13:09:29 -0400

    Display liquid templates

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-use std::error::Error;
+use async_std::fs::read_to_string;
+use liquid::Object;
+use std::{error::Error, str::FromStr};
+use tide::{http::Mime, Response, StatusCode};
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut app = tide::new();
 
-    app.at("/").get(|_| async { Ok("Hello from Tide") });
+    app.at("/").get(|_| async {
+        let path = "./templates/index.html.liquid";
+        let source = read_to_string(path).await.unwrap();
+        let compiler = liquid::ParserBuilder::with_stdlib().build().unwrap();
+        let template = compiler.parse(&source).unwrap();
+        let globals: Object = Default::default();
+        let markup = template.render(&globals).unwrap();
+        let mut res = Response::new(StatusCode::Ok);
+        res.set_content_type(Mime::from_str("text/html; charset=utf-8").unwrap());
+        res.set_body(markup);
+        Ok(res)
+    });
 
     app.listen("localhost:3000").await?;
     Ok(())
