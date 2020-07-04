d6a1a060a47454c555a1a833ce754b4718bf3d01
Fernando Paredes <nano@fdp.io>
2020-07-03 20:07:08 -0400

    Refactor template serving and error handling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use image::{imageops::FilterType, jpeg::JPEGEncoder, DynamicImage, GenericImageV
 use liquid::{Object, Template};
 use rand::Rng;
 use serde::Serialize;
-use std::{collections::HashMap, error::Error, net::SocketAddr, sync::Arc};
+use std::{collections::HashMap, convert::Infallible, error::Error, net::SocketAddr, sync::Arc};
 use tide::{http::Mime, Request, Response, StatusCode};
 use tokio::{fs::read_to_string, sync::RwLock};
 use ulid::Ulid;
@@ -65,12 +65,6 @@ struct Image {
     contents: Vec<u8>,
 }
 
-trait BitCrush: Sized {
-    type Error;
-
-    fn bitcrush(self) -> Result<Self, Self::Error>;
-}
-
 trait MimeAware {
     fn content_type(self, mime: Mime) -> Self;
 }
@@ -79,6 +73,39 @@ impl MimeAware for http::response::Builder {
     fn content_type(self, mime: Mime) -> Self {
         self.header("content-type", mime.to_string())
     }
+}
+
+trait ForWarp {
+    type Reply;
+
+    fn for_warp(self) -> Result<Self::Reply, warp::Rejection>;
+}
+
+impl<T> ForWarp for Result<T, Box<dyn Error>>
+where
+    T: warp::Reply + 'static,
+{
+    type Reply = Box<dyn warp::Reply>;
+
+    fn for_warp(self) -> Result<Self::Reply, warp::Rejection> {
+        let b: Box<dyn warp::Reply> = match self {
+            Ok(reply) => Box::new(reply),
+            Err(e) => {
+                log::error!("Error: {}", e);
+                let res = http::Response::builder()
+                    .status(500)
+                    .body("Something went wrong, sorry");
+                Box::new(res)
+            }
+        };
+        Ok(b)
+    }
+}
+
+trait BitCrush: Sized {
+    type Error;
+
+    fn bitcrush(self) -> Result<Self, Self::Error>;
 }
 
 impl BitCrush for DynamicImage {
@@ -133,19 +160,23 @@ async fn compile_templates(paths: &[&str]) -> Result<TemplateMap, Box<dyn Error>
 }
 
 async fn serve_template(
-    templates: &TemplateMap,
+    state: &State,
     name: &str,
     mime: Mime,
-) -> Result<Response, Box<dyn Error>> {
-    let template = templates
+) -> Result<impl warp::Reply, Box<dyn Error>> {
+    let template = state
+        .templates
         .get(name)
         .ok_or_else(|| TemplateError::TemplateNotFound(name.to_string()))?;
+
     let globals: Object = Default::default();
-    let markup = template.render(&globals).unwrap();
-    let mut res = Response::new(StatusCode::Ok);
-    res.set_content_type(mime);
-    res.set_body(markup);
-    Ok(res)
+    let markup = template.render(&globals)?;
+
+    Ok(http::Response::builder().content_type(mime).body(markup))
+    // let mut res = Response::new(StatusCode::Ok);
+    // res.set_content_type(mime);
+    // res.set_body(markup);
+    // Ok(res)
 }
 
 #[tokio::main]
@@ -175,31 +206,32 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let index = warp::filters::method::get()
         .and(warp::path::end())
         .and(with_state())
-        .map(|state: Arc<State>| {
-            let template = state.templates.get("index.html").unwrap();
-            let globals: Object = Default::default();
-            let markup = template.render(&globals).unwrap();
-
-            http::Response::builder()
-                .content_type(mimes::html())
-                .body(markup)
+        .and_then(|state: Arc<State>| async move {
+            serve_template(&state, "index.html", mimes::html())
+                .await
+                .for_warp()
         });
 
     let style = warp::filters::method::get()
         .and(warp::path!("style.css"))
         .and(with_state())
-        .map(|state: Arc<State>| {
-            let template = state.templates.get("style.css").unwrap();
-            let globals: Object = Default::default();
-            let markup = template.render(&globals).unwrap();
+        .and_then(|state: Arc<State>| async move {
+            serve_template(&state, "style.css", mimes::css())
+                .await
+                .for_warp()
+        });
 
-            http::Response::builder()
-                .content_type(mimes::css())
-                .body(markup)
+    let js = warp::filters::method::get()
+        .and(warp::path!("main.js"))
+        .and(with_state())
+        .and_then(|state: Arc<State>| async move {
+            serve_template(&state, "main.js", mimes::js())
+                .await
+                .for_warp()
         });
 
     let addr: SocketAddr = "127.0.0.1:3000".parse()?;
-    warp::serve(index.or(style)).run(addr).await;
+    warp::serve(index.or(style).or(js)).run(addr).await;
     Ok(())
     // let mut app = tide::with_state(state);
 
